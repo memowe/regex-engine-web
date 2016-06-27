ea03d6ae1fb6028875fe65917165fe59fb7a2dd5
Mirko Westermeier <mirko.westermeier@uni-muenster.de>
2016-06-27 15:59:48 +0200

    implement first basic proof-of-concept web matcher

--- regex-engine-web.pl
+++ regex-engine-web.pl
@@ -1,27 +1,70 @@
 #!/usr/bin/env perl
+
+use FindBin '$Bin';
+use lib "$Bin/regex-engine/lib";
+use REE;
 use Mojolicious::Lite;
 
-# Documentation browser under "/perldoc"
-plugin 'PODRenderer';
+# regex form (template only)
+any '/' => 'home';
 
-get '/' => sub {
-  my $c = shift;
-  $c->render(template => 'index');
+# information about a match
+post '/match' => sub {
+    my $c = shift;
+
+    # form data
+    my $regex   = $c->param('regex');
+    my $input   = $c->param('input');
+
+    # try to compile
+    my $ree     = eval {REE->new(regex => $regex)};
+    return $c->render(text => $@) if $@;
+
+    # done
+    $c->stash(
+        regex   => $regex,
+        input   => $input,
+        success => $ree->match($input) ? 1 : 0,
+        ree     => $ree,
+    );
 };
 
 app->start;
 __DATA__
 
-@@ index.html.ep
+@@ home.html.ep
 % layout 'default';
-% title 'Welcome';
-<h1>Welcome to the Mojolicious real-time web framework!</h1>
-To learn more, you can browse through the documentation
-<%= link_to 'here' => '/perldoc' %>.
+% title 'Regular Expression Engine Web';
+<h1><%= title %></h1>
+%= form_for match => (method => 'post') => begin
+    <p>
+        %= label_for regex => 'Regular Expression:'
+        %= text_field regex => param('regex') => (id => 'regex')
+        %= label_for input => 'Input:'
+        %= text_field input => param('input') => (id => 'input')
+    </p>
+    <p><%= submit_button 'Try to match!' %></p>
+% end
+
+@@ match.html.ep
+% layout 'default';
+% title 'REE Matching Result';
+<h1><%= title %></h1>
+<table>
+    <tr><th>Regular Expression</th><td><code><%= $regex %></code></td></tr>
+    <tr><th>Input</th><td><code><%= $input %></code></td></tr>
+    <tr><th>Match</th><td><%= $success ? 'Yes' : 'No' %></td></tr>
+</table>
+<p>NFA:</p>
+<pre><code><%= $ree->nfa_representation %></code></pre>
+%= form_for home => (method => 'post') => begin
+    %= hidden_field regex => $regex
+    %= hidden_field input => $input
+    %= submit_button 'Try again!'
+% end
 
 @@ layouts/default.html.ep
 <!DOCTYPE html>
-<html>
-  <head><title><%= title %></title></head>
-  <body><%= content %></body>
-</html>
+<html><head><title><%= title %></title></head><body>
+%= content
+</body></html>
---
+++ t/match.t
@@ -0,0 +1,35 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+
+use Test::More tests => 17;
+use Test::Mojo;
+
+# prepare
+use FindBin '$Bin';
+use lib "$Bin/../regex-engine/lib";
+require "$Bin/../regex-engine-web.pl";
+my $t = Test::Mojo->new;
+
+# test form
+$t->get_ok('/')->status_is(200)->text_is(h1 => 'Regular Expression Engine Web')
+    ->element_exists('form input[name=regex]')
+    ->element_exists('form input[name=input]')
+    ->element_exists('form input[type=submit]');
+
+# wrong regex
+$t->post_ok('/match' => form => {regex => '*', input => 'foo'})
+    ->status_is(200)->content_like(qr/Parse error: unexpected \*/);
+
+# no match
+$t->post_ok('/match' => form => {regex => 'a', input => 'b'})->status_is(200)
+    ->text_is('tr:nth-child(3) th', 'Match')
+    ->text_is('tr:nth-child(3) td', 'No');
+
+# match
+$t->post_ok('/match' => form => {regex => 'a', input => 'a'})->status_is(200)
+    ->text_is('tr:nth-child(3) th', 'Match')
+    ->text_is('tr:nth-child(3) td', 'Yes');
+
+__END__
